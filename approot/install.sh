3968c98d03b8bae840d16a73d2831d306560d137
uxff <uxff@qq.com>
2017-11-28 09:53:14 +0800

    fix install.sh

--- approot/install.sh
+++ approot/install.sh
@@ -4,3 +4,6 @@ chmod a+w temp/ -R
 
 chmod a+w upload/ -R
 chmod a+w protected/runtime/ -R
+
+mv protected/xahoomob/config/main.php.bak protected/xahoomob/config/main.php
+mv protected/xahooadmin/config/main.php.bak protected/xahooadmin/config/main.php
